e1fb11b9501ac917ec1035bdde2f84c355cc0626
Alex MacArthur <alex@macarthur.me>
2024-07-21 16:17:03 -0500

    Add tests, bump version.

--- spec/lib/service_spec.rb
+++ spec/lib/service_spec.rb
@@ -145,4 +145,90 @@ describe JamComments::Service do
       end
     end
   end
+
+  context "custom copy" do
+    it "passes copy" do
+      instance = described_class.new(
+        domain: "example.com",
+        api_key: "abc123",
+        copy: {
+          copy_confirmation_message: "Thanks for commenting!",
+          copy_submit_button: "Post",
+          copy_name_placeholder: "Name",
+          copy_email_placeholder: "Email",
+          copy_comment_placeholder: "Comment",
+          copy_write_tab: "Write",
+          copy_preview_tab: "Preview",
+          copy_auth_button: "Log in",
+          copy_log_out_button: "Log out",
+        },
+        client: client
+      )
+
+      expect(client).to receive(:get).with(
+        "https://go.jamcomments.com/api/v3/markup",
+        {
+          query: hash_including(
+            path: "/path",
+            domain: "example.com",
+            stub: nil,
+            copy_confirmation_message: "Thanks for commenting!",
+            copy_submit_button: "Post",
+            copy_name_placeholder: "Name",
+            copy_email_placeholder: "Email",
+            copy_comment_placeholder: "Comment",
+            copy_write_tab: "Write",
+            copy_preview_tab: "Preview",
+            copy_auth_button: "Log in",
+            copy_log_out_button: "Log out"
+          ),
+          headers: hash_including(
+            Authorization: "Bearer abc123",
+            Accept: "application/json",
+            "X-Platform": "bridgetown"
+          ),
+        }
+      ).and_return(OpenStruct.new(
+                     code: 200,
+                     body: "html!"
+                   ))
+
+      instance.fetch(path: "/path")
+    end
+
+    it "passes only some copy" do
+      instance = described_class.new(
+        domain: "example.com",
+        api_key: "abc123",
+        copy: {
+          copy_confirmation_message: "Thanks for commenting!",
+          copy_submit_button: "Post",
+        },
+        client: client
+      )
+
+      expect(client).to receive(:get).with(
+        "https://go.jamcomments.com/api/v3/markup",
+        {
+          query: hash_including(
+            path: "/path",
+            domain: "example.com",
+            stub: nil,
+            copy_confirmation_message: "Thanks for commenting!",
+            copy_submit_button: "Post"
+          ),
+          headers: hash_including(
+            Authorization: "Bearer abc123",
+            Accept: "application/json",
+            "X-Platform": "bridgetown"
+          ),
+        }
+      ).and_return(OpenStruct.new(
+                     code: 200,
+                     body: "html!"
+                   ))
+
+      instance.fetch(path: "/path")
+    end
+  end
 end
